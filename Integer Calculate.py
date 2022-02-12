c532bb55a080b63eaf9f31f64beb5c6bae444a29
JaeyunLim <97609649+JaeyunLim@users.noreply.github.com>
2022-02-12 16:04:16 +0900

    정수형 숫자 계산

---
+++ Integer Calculate.py
@@ -0,0 +1,15 @@
+#integer plus actual number
+num1 = 200
+num2 = 300
+print("answer is %.3f" %(num1 + num2))
+
+#integer squared
+num3 = 3
+print(num3**4)
+
+#printing integer
+num4 = 27
+num5 = 28
+print("answer is %d" %(num4%2))
+print("answer is %d" %(num5%2))
+
