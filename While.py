406d13ccafe724a73f2d0b990638ad5c37ba730d
JaeyunLim <97609649+JaeyunLim@users.noreply.github.com>
2022-02-12 16:05:44 +0900

    While문

---
+++ While.py
@@ -0,0 +1,20 @@
+#Treehit
+Treehit = 0
+while Treehit < 10:
+    Treehit  = Treehit + 1
+    print("Hit %d times."%Treehit)
+    if Treehit == 10:
+        print("Tree is going down.")
+
+print("---------------------------------------------")
+
+count_4 = 0
+while count_4 < 10:
+    count_4 = count_4 + 1
+    if count_4 % 2 == 0:
+        continue
+    print(count_4)
+
+print("---------------------------------------------")
+
+
