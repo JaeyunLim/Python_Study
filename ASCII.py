4c3b15081f788182755977b317be6754db2e4d4a
JaeyunLim <97609649+JaeyunLim@users.noreply.github.com>
2022-02-12 16:02:58 +0900

    아스키 코드
    2022 02 12

---
+++ ASCII.py
@@ -0,0 +1,10 @@
+#ASCII code num -> chr
+print("answer is %c" %chr(65))
+#ASCII code chr -> num
+print("answer is %d" %ord('D'))
+
+print("answer is %c"%chr(49))
+print("answer is %d"%ord('T'))
+
+print("a = %d"%ord('-'))
+print("a = %s"%chr(32))
