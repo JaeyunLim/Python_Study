ee5bd3927e9c5c186effd37781e75f08cbecd6bd
JaeyunLim <97609649+JaeyunLim@users.noreply.github.com>
2022-03-05 11:52:57 +0900

    Update While.py

--- While.py
+++ While.py
@@ -1,13 +1,3 @@
-#Treehit
-Treehit = 0
-while Treehit < 10:
-    Treehit  = Treehit + 1
-    print("Hit %d times."%Treehit)
-    if Treehit == 10:
-        print("Tree is going down.")
-
-print("---------------------------------------------")
-
 count_4 = 0
 while count_4 < 10:
     count_4 = count_4 + 1
